ca18a34734c3ccbfe9dce7c541e98670fd2b61f7
Devexpat <devexpat@gmail.com>
2019-09-09 14:35:02 +0000

    initial renamed app - Kouterp

---
+++ addons/l10n_pl/migrations/9.0.2.0/post-migrate_tags_on_taxes.py
@@ -0,0 +1,6 @@
+from Kouterp.modules.registry import RegistryManager
+
+def migrate(cr, version):
+    registry = RegistryManager.get(cr.dbname)
+    from Kouterp.addons.account.models.chart_template import migrate_tags_on_taxes
+    migrate_tags_on_taxes(cr, registry)
